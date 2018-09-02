171e0eb6274fc06e01e7b09524fd332c98e3b0bc
Umer Jan <iumerjan>
2018-09-02 18:22:19 +0530

    1. Setup Environment variable for global access to API Url. 2. Add getcars service for fetching all cars.

--- src/app/services/cars.service.ts
+++ src/app/services/cars.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
 
+import { HttpClient, HttpResponse, HttpHeaders, HttpRequest } from '@angular/common/http';
 @Injectable()
 export class CarsService {
 
-  constructor() { }
+  baseURL = environment.baseURL + '/api/car/';
+  constructor(private httpClient: HttpClient) { }
+
+  getCars() {
+
+    return this.httpClient.get(this.baseURL);
+
+  }
+
+
 
 }
