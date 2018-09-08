cbff09c514762dbed7dc8242952c9c7d19190b9f
Umer Jan <iumerjan>
2018-09-08 13:44:58 +0530

    Delete Car Component

---
+++ src/app/pages/delete-car/delete-car.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-car',
+  templateUrl: './delete-car.component.html',
+  styleUrls: ['./delete-car.component.css']
+})
+export class DeleteCarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
